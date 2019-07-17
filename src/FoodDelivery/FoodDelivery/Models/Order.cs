062f32c637b2be34cdd728bbecfa140a94d71b4a
kelvintrinh174 <kelvintrinh174@gmail.com>
2019-07-17 14:43:46 -0400

    Update OrderController, Order Model and Views/Order

--- src/FoodDelivery/FoodDelivery/Models/Order.cs
+++ src/FoodDelivery/FoodDelivery/Models/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FoodDelivery.Models
 {
@@ -10,21 +12,45 @@ namespace FoodDelivery.Models
             TblOrderItem = new HashSet<OrderItem>();
         }
 
+        [Key]
         public int IdOrder { get; set; }
+        [DisplayName("Customer Name")]
+        [Required(ErrorMessage = "Customer Name is Required.")]
         public int IdCustomer { get; set; }
+
+        [DisplayName("Address Name")]
+        [Required(ErrorMessage = "Address Name is Required.")]
         public int IdAddress { get; set; }
+
+        [DisplayName("Payment Method")]
+        [Required(ErrorMessage = "Payment Method is Required.")]
         public int IdPaymentMethod { get; set; }
+        [DisplayName("Restaurant Name")]
+        [Required(ErrorMessage = "Restaurant Name is Required.")]
         public int IdRestaurant { get; set; }
-        public int IdOrderStatus { get; set; }
-        public decimal OrderNetAmount { get; set; }
-        public decimal OrderTax { get; set; }
-        public decimal OrderGrossAmount { get; set; }
+        [DisplayName("Order Status Name")]
+        [Required(ErrorMessage = "Order Status is Required.")]
+        public int? IdOrderStatus { get; set; }
+        [DisplayName("Order Net Amount")]
+        [Required(ErrorMessage = "Order Net Amount is Required.")]
+        public decimal? OrderNetAmount { get; set; }
+        [DisplayName("Order Tax")]
+        [Required(ErrorMessage = "Order Tax is Required.")]
+        public decimal? OrderTax { get; set; }
+        [DisplayName("Order Gross Amount")]
+        [Required(ErrorMessage = "Order Gross Amount is Required.")]
+        public decimal? OrderGrossAmount { get; set; }
         public string CustomerNotes { get; set; }
 
+        [DisplayName("Address")]
         public virtual DeliveryAddress AddressNavigation { get; set; }
+        [DisplayName("Customer Name")]
         public virtual Customer CustomerNavigation { get; set; }
+        [DisplayName("Order Status")]
         public virtual OrderStatus OrderStatusNavigation { get; set; }
+        [DisplayName("Payment Method")]
         public virtual PaymentMethod PaymentMethodNavigation { get; set; }
+        [DisplayName("Restaurant Name")]
         public virtual Restaurant RestaurantNavigation { get; set; }
         public virtual ICollection<OrderItem> TblOrderItem { get; set; }
     }
